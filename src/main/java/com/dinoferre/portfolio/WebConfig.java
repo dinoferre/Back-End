3991a6660ac05d8394783218c4becbef2df580af
Dino Ferre <dinoferre@hotmail.com>
2023-04-24 18:59:23 -0300

    modifico security

--- src/main/java/com/dinoferre/portfolio/WebConfig.java
+++ src/main/java/com/dinoferre/portfolio/WebConfig.java
@@ -1,4 +1,11 @@
-
+/**
+ * La clase WebConfig implementa la interfaz WebMvcConfigurer y anotada con @Configuration. 
+ * Esta clase contiene el método addCorsMappings que se utiliza para agregar configuraciones de CORS 
+ * (Cross-Origin Resource Sharing) al registro de CORS. En este caso, la configuración registry.addMapping("/**") 
+ * permite el acceso CORS a cualquier URL.
+ * 
+ * @author Dino Ferré
+ **/
 package com.dinoferre.portfolio;
 
 import org.springframework.context.annotation.Configuration;
